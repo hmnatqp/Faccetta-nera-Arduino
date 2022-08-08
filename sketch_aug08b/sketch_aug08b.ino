1141a58b25a89a7ae73749779a44cbe27642aad2
hmnatqp <110863707+hmnatqp@users.noreply.github.com>
2022-08-09 01:31:03 +0200

    Add files via upload

---
+++ sketch_aug08b/sketch_aug08b.ino
@@ -0,0 +1,96 @@
+#include "pitches.h"
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+
+ //1
+ 
+  tone(8, 880, 220);
+  delay(240);
+
+  tone(8, 880, 220);
+  delay(240);
+  
+  tone(8, 880, 220);
+  delay(240);
+
+  tone(8, 830, 460);
+  delay(480);
+
+  tone(8, 740, 100);
+  delay(120);
+
+  tone(8, 660, 580);
+  delay(600);
+  
+//2
+ 
+  tone(8, 1176, 220);
+  delay(240);
+ 
+  tone(8, 1176, 220);
+  delay(240);
+
+  tone(8, 1176, 220);
+  delay(240);
+  
+  tone(8, 1108, 460);
+  delay(480);
+
+  tone(8, 988, 100);
+  delay(120);
+
+  tone(8, 880, 580);
+  delay(600);
+
+//3
+
+ 
+  tone(8, 1320, 220);
+  delay(240);
+
+  tone(8, 1320, 220);
+  delay(240);
+
+  tone(8, 1320, 220);
+  delay(240);
+
+  tone(8, 1320, 460);
+  delay(480);
+
+  tone(8, 1108, 100);
+  delay(120);
+ 
+  tone(8, 1108, 220);
+  delay(240);
+
+  tone(8, 880, 220);
+  delay(240);
+
+//4
+
+  tone(8, 880, 460);
+  delay(480);
+
+  tone(8, 988, 220);
+  delay(240);
+  
+  tone(8, 880, 220);
+  delay(240);
+
+  tone(8, 830, 460);
+  delay(480);
+
+  tone(8, 740, 100);
+  delay(120);
+
+  tone(8, 660, 580);
+  delay(600);
+  
+delay(2000);
+}
